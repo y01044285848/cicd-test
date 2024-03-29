49b19d3189bf11f6522040e55c76e33a1de9f481
y01044285848 <y01044285848@gmail.com>
2024-03-29 15:31:47 +0900

    cicd-test 1

---
+++ src/main/java/kr/co/cicd/controller/MainController.java
@@ -0,0 +1,22 @@
+package kr.co.cicd.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@RequiredArgsConstructor
+@Controller
+public class MainController {
+
+    private final BuildProperties buildProperties;
+
+    @GetMapping("/")
+    public String index(Model model){
+        String version = buildProperties.getVersion();
+        model.addAttribute(version);
+
+        return "/index";
+    }
+
+}
